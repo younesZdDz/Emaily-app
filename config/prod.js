f7fe849edd69d2e6fe7510d3b7cdd2fd45e452fa
younesZdDz <32100131+younesZdDz@users.noreply.github.com>
2018-02-28 00:59:15 +0100

    small tweak

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //prod.js -- production keys here
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: rocess.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: rocess.env.MONGO_URI,
-  cookieKey : rocess.env.COOKIE_KEY
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey : process.env.COOKIE_KEY
 };
